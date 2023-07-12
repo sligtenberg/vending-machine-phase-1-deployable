f650c7c47400f4efd6697cef92b83761a6733fce
stephen ligtenberg <bountyhunter24@gmail.com>
2023-07-11 23:29:13 -0400

    production ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :cashes, only: [:index, :update]
-  resources :snacks, only: [:index, :update]
+  #namespace :api do
+    resources :cashes, only: [:index, :update]
+    resources :snacks, only: [:index, :update]
+  #end
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end

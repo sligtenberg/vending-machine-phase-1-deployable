d467f83a7133a3223d691d3c94cb981c3965ddcd
stephen ligtenberg <bountyhunter24@gmail.com>
2023-07-13 12:50:31 -0400

    using host variable

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  #namespace :api do
-    resources :cashes, only: [:index, :update]
-    resources :snacks, only: [:index, :update]
-  #end
+  resources :cashes, only: [:index, :update]
+  resources :snacks, only: [:index, :update]
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
